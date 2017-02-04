f4b557141803c78eea00d143a69cf44c47a03d1d
AhmadAlMaradni <ahmad.maradni@hotmail.com>
2017-02-04 09:42:40 +0200

    part3

--- node-express-sequelize/server.js
+++ node-express-sequelize/server.js
@@ -3,14 +3,23 @@ var app = express();
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize('test', 'test', null, { dialect: 'sqlite', storage: './db.test.sqlite' });
 
+// var router  = express.Router();
+
 var User = sequelize.define('User', {
   username: Sequelize.STRING
 });
 
 /*  Create a '/users' route that responds to 
     a GET request with all users in the database */
+
+    // get request for index page
 app.get('/',function(req , res){
-	res.send('user in')
+	res.send('user')
+})
+
+   // get request for /users page
+app.get('/users',function(req , res){
+	res.send('all user in')
 })
 
 module.exports = { 
